ad3cea8a7fa1e43e33e1e8e388279fbaac404fd4
Lokesh Bhaskar <139795809+LokeshBhaskarNR@users.noreply.github.com>
2024-12-01 09:55:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,8 @@ def save_activity(username, image_path, prediction):
 
 # Display user activity
 def display_saved_activities(username):
-    st.markdown(f"Monitor Your Activity, {st.session_state.global_username}")
+    st.title(f"📊 Monitor Your Activity, {st.session_state.global_username}")
+
     conn = sqlite3.connect("dermaevolve.db")
     cursor = conn.cursor()
     cursor.execute("""
